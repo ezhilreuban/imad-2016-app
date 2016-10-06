ef766c6107e68cd8bb1ce2c75537b451119e463a
ezhilreuban <ezhilkreuban@gmail.com>
2016-10-06 15:35:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1'), function (req, res) {
-  res.sendFile('article1 is requested')
-}
+  res.sendFile('article1 is requested'));
+});
 app.get('/article2'), function (req, res) {
-  res.sendFile('article2 is requested')
-}
+  res.sendFile('article2 is requested'));
+});
 app.get('/article3'), function (req, res) {
-  res.sendFile('article3 is requested')
-}
+  res.sendFile('article3 is requested'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
